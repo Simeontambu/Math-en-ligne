19426d96767167e8520f3cbdedebcfb07d5b132d
simeontambu <simeontambu@gmail.com>
2023-06-09 15:33:54 +0100

    some comment

--- second.js
+++ second.js
@@ -1,28 +1,39 @@
-const buttons= document.querySelectorAll(".resolution");
+const buttons = document.querySelectorAll(".resolution");
 
 for (const buttonss of buttons) {
   buttonss.addEventListener("click", delta);
 }
-function delta(){
+function delta() {
+  //Retrieve the value of an input
+  let valueOfA = document.getElementById("valueOfA").value;
+  let valueOfB = document.getElementById("valueOfB").value;
+  let valueOfC = document.getElementById("valueOfC").value;
 
-    //Recuperer la valeur d'un input
-    let a = document.getElementById("putA").value;
-    let b = document.getElementById("putB").value;
-    let c = document.getElementById("putC").value;
-
-    let delta= ((b*b) -(4*a*c));
-    if ((!isNaN(a) && a !="") && (!isNaN(b) && b !="") && (!isNaN(c) && c !=""))
-    {
-        if (delta < 0){
-            document.formulaire.resultat.value = "Pas de solution";
-        }else if (delta > 0){
-            document.formulaire.resultat.value ="X1= "+ (-b-delta)/(2*a) +" et "+ " X2= " + (-b+delta)/(2*a);
-        }else{
-            document.formulaire.resultat.value ="X1=X2= " + (-b/(2*a));
-        }
-    }else{
-        messageerreur.style.color="#FF0000"
-        messageerreur.innerText = "Veillez saisir les valeurs numeriques";
+  // formula for calculating quadratic equation
+  let delta = (valueOfB * valueOfB) - 4 * valueOfA * valueOfC;
+  if (
+    !isNaN(valueOfA) &&
+    valueOfA != "" &&
+    !isNaN(valueOfB) &&
+    valueOfB != "" &&
+    !isNaN(valueOfC) &&
+    valueOfC != ""
+  ) {
+    if (delta < 0) {
+      document.formulaire.resultat.value = "Pas de solution";
+    } else if (delta > 0) {
+      document.formulaire.resultat.value =
+        "X1= " +
+        (-valueOfB - delta) / (2 * valueOfA) +
+        " et " +
+        " X2= " +
+        (-valueOfB + delta) / (2 * valueOfA);
+    } else {
+      document.formulaire.resultat.value =
+        "X1=X2= " + -valueOfB / (2 * valueOfA);
     }
-    
+  } else {
+    messageerreur.style.color = "#FF0000";
+    messageerreur.innerText = "Veillez saisir les valeurs numeriques";
+  }
 }
